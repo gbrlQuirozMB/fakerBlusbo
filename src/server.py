b79f8f5f3454afcfcac03cb6acf89dd7c66e2042
Gabriel Quiroz <gabriel@mb.company>
2020-09-15 12:44:18 -0500

    TOREVIEW-> endpoint creados en faker se parametriza rebajas, se quita oto-invierno

--- src/server.py
+++ src/server.py
@@ -34,32 +34,141 @@ api = Flask(__name__)
 
 @api.route('/menu', methods=['GET'])
 def getMenu():
+    # r = Response(json.dumps(menu), status=200)
     with open('menu.json', 'r') as j:
         jsonData = json.load(j)
-    # r = Response(json.dumps(menu), status=200)
     r = Response(json.dumps(jsonData), status=200)
     r.headers["Content-Type"] = "application/json; charset=utf-8"
     return r
 
 
-@api.route('/detalle-producto/1', methods=['GET'])
-def getDetalleProducto():
-    # r = Response(json.dumps(detalleProducto), status=200)
-    with open('detalle-producto.json', 'r') as j:
-        jsonData = json.load(j)
-    r = Response(json.dumps(jsonData), status=200)
+@api.route('/detalle-producto/<id>', methods=['GET'])
+def getDetalleProducto(id):
+    if id != '1':
+        r = Response(json.dumps(notFound), status=404)
+    else:
+        with open('detalle-producto.json', 'r') as j:
+            jsonData = json.load(j)
+        r = Response(json.dumps(jsonData), status=200)
+
     r.headers["Content-Type"] = "application/json; charset=utf-8"
     return r
 
 
-@api.route('/detalle-producto/recomendados/B68496', methods=['GET'])
-def getDetalleProductoRecomendados():
-    # r = Response(json.dumps(detalleProductoRecomendados), status=200)
-    with open('detalle-producto-recomendados.json', 'r') as j:
-        jsonData = json.load(j)
-    r = Response(json.dumps(jsonData), status=200)
+@api.route('/detalle-producto/recomendados/<sku>', methods=['GET'])
+def getDetalleProductoRecomendados(sku):
+    if sku != 'B68496':
+        r = Response(json.dumps(notFound), status=404)
+    else:
+        with open('detalle-producto-recomendados.json', 'r') as j:
+            jsonData = json.load(j)
+        r = Response(json.dumps(jsonData), status=200)
+
     r.headers["Content-Type"] = "application/json; charset=utf-8"
     return r
+
+# ---------
+
+
+@api.route('/hombre/temporada/<id>', methods=['GET'])
+def getHombre(id):
+    if id != '1':
+        r = Response(json.dumps(notFound), status=404)
+    else:
+        with open('hombre-temporada.json', 'r') as j:
+            jsonData = json.load(j)
+        r = Response(json.dumps(jsonData), status=200)
+
+    r.headers["Content-Type"] = "application/json; charset=utf-8"
+    return r
+
+
+@api.route('/hombre/temporada/<idTemporada>/categoria/<idCategoria>', methods=['GET'])
+def getHombreCategoria(idTemporada, idCategoria):
+    if idTemporada != '1' or idCategoria != '1':
+        r = Response(json.dumps(notFound), status=404)
+    else:
+        with open('hombre-temporada-categoria.json', 'r') as j:
+            jsonData = json.load(j)
+        r = Response(json.dumps(jsonData), status=200)
+
+    r.headers["Content-Type"] = "application/json; charset=utf-8"
+    return r
+
+
+@api.route('/mujer/temporada/<id>', methods=['GET'])
+def getMujer(id):
+    if id != '1':
+        r = Response(json.dumps(notFound), status=404)
+    else:
+        with open('mujer-temporada.json', 'r') as j:
+            jsonData = json.load(j)
+        r = Response(json.dumps(jsonData), status=200)
+
+    r.headers["Content-Type"] = "application/json; charset=utf-8"
+    return r
+
+
+@api.route('/mujer/temporada/<idTemporada>/categoria/<idCategoria>', methods=['GET'])
+def getMujerCategoria(idTemporada, idCategoria):
+    if idTemporada != '1' or idCategoria != '1':
+        r = Response(json.dumps(notFound), status=404)
+    else:
+        with open('mujer-temporada-categoria.json', 'r') as j:
+            jsonData = json.load(j)
+        r = Response(json.dumps(jsonData), status=200)
+
+    r.headers["Content-Type"] = "application/json; charset=utf-8"
+    return r
+
+
+@api.route('/mujer/temporada/<id>/categorias', methods=['GET'])
+def getMujerCategorias(id):
+    if id != '1':
+        r = Response(json.dumps(notFound), status=404)
+    else:
+        with open('mujer-temporada-categorias.json', 'r') as j:
+            jsonData = json.load(j)
+        r = Response(json.dumps(jsonData), status=200)
+
+    r.headers["Content-Type"] = "application/json; charset=utf-8"
+    return r
+
+@api.route('/rebajas/<id>', methods=['GET'])
+def getRebajas(id):
+    if id != '1':
+        r = Response(json.dumps(notFound), status=404)
+    else:
+        with open('rebajas.json', 'r') as j:
+            jsonData = json.load(j)
+        r = Response(json.dumps(jsonData), status=200)
+
+    r.headers["Content-Type"] = "application/json; charset=utf-8"
+    return r
+
+
+@api.route('/rebajas/temporada/<idTemporada>/categoria/<idCategoria>', methods=['GET'])
+def getRebajasTemporada(idTemporada, idCategoria):
+    if idTemporada != '1' or idCategoria != '1':
+        r = Response(json.dumps(notFound), status=404)
+    else:
+        with open('rebajas-categoria.json', 'r') as j:
+            jsonData = json.load(j)
+        r = Response(json.dumps(jsonData), status=200)
+
+    r.headers["Content-Type"] = "application/json; charset=utf-8"
+    return r
+
+
+# ---------FALTAN POR DEFINIR
+
+# @api.route('/completa-compra', methods=['GET'])
+# def getCompletaCompra():
+#     with open('completa-compra.json', 'r') as j:
+#         jsonData = json.load(j)
+#     r = Response(json.dumps(jsonData), status=200)
+#     r.headers["Content-Type"] = "application/json; charset=utf-8"
+#     return r
 
 
 @api.route('/prueba', methods=['POST'])
@@ -75,8 +184,6 @@ def postPrueba():
 
     r.headers["Content-Type"] = "application/json; charset=utf-8"
     return r
-
-# ---------FALTAN POR DEFINIR
 
 
 @api.route('/detalle-producto', methods=['POST'])
